c03651b5de00c907b96c15043425654f4d5373d3
Andre Draghetti <drego85@draghetti.it>
2019-03-26 18:16:08 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# meioc
-Extracting IOC data from an eMail
+# Meioc
+
+Meioc (Mail Extractor IoC) is a python3 script to extract indicators of compromised from eMail.
+
+Meioc allows you to extract the following information from an e-mail, in JSON format:
+
+* Header Field: From
+* Header Field: Sender
+* Header Field: Subject
+* Header Field: X-Originating-IP
+* Relay Full
+* Relay IP (Only the IPs involved with the possibility of excluding private IPs.)
+* Urls
+* Domains
+
+The software is currently distributed in beta version, every collaboration is welcome.
+
+### To Do List
+
+- [ ] Scan multiple eMail (directory input)
+- [ ] Whitelist
+
+### Requirements
+
+pip3 install requirementes.txt
+
+### Example
+
+$ python3 meioc.py email.eml -x
+
+```json
+{
+    "data": [
+        {
+            "Filename": "phishing1.eml",
+            "From": "info@example.com",
+            "Sender": "",
+            "Subject": "Phishing Mail",
+            "X-Originating-IP": "",
+            "relay_full": [
+                {
+                    "0": "localhost.localdomain (unknown [AAA.BBB.CCC.DDD])",
+                    "1": "v6791.vps.example.ru (unknown [127.0.0.1])",
+                    "2": "v6791.vps.example.ru ([QQQ.WWW.EEE.RRR])",
+                    "3": "mail.it ([10.103.10.23])",
+                    "4": "dcp-11.mail.local ([10.103.10.23])",
+                    "5": "dcd-16 ([10.103.10.23])"
+                }
+            ],
+            "relay_ip": [
+                {
+                    "0": "AA.BBB.CCC.DDD",
+                    "1": "QQQ.WWW.EEE.RRR"
+                }
+            ],
+            "url": [
+                {
+                    "0": "https://www.example.com/en/private.html",
+                    "1": "http://phishingsite.random.net"
+                }
+            ],
+            "domain": [
+                {
+                    "0": "example.it",
+                    "1": "random.net"
+                }
+            ]
+        }
+    ]
+}
+```
+
+### License
+
+GNU General Public License v3.0
---
+++ meioc.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+# This file is part of Meioc.
+#
+# Meioc was made with ♥ by Andrea Draghetti
+#
+# This file may be licensed under the terms of of the
+# GNU General Public License Version 3 (the ``GPL'').
+#
+import os
+import re
+import sys
+import json
+import argparse
+import ipaddress
+import tldextract
+from email import policy
+from urlextract import URLExtract
+from email.parser import BytesParser
+
+tldcache = tldextract.TLDExtract(cache_file="./.tld_set")
+
+
+def email_analysis(filename, exclude_private_ip):
+    urlList = []
+    domainList = []
+    hopList = []
+    hopListIP = []
+    data = {}
+    data["data"] = []
+
+    with open(filename, "rb") as fp:
+        msg = BytesParser(policy=policy.default).parse(fp)
+
+    if msg:
+        # Identify each url reported in the eMail body
+        for part in msg.walk():
+            if part.get_content_type() == "text/plain" or part.get_content_type() == "text/html":
+                extractor = URLExtract()
+                urlList.extend(extractor.find_urls(part.get_content()))
+
+        # Identify each domain reported in the eMail body
+        for url in urlList:
+            analyzeddomain = tldcache(url).registered_domain
+            if analyzeddomain:
+                domainList.append(analyzeddomain)
+
+        # Remove Duplicate
+        urlList = list(set(urlList))
+        domainList = list(set(domainList))
+
+        # A sender obfuscation technique involves entering two e-mails. Only the last one is the real one. Example:
+        #
+        # Sender Name: Mario Rossi <rossi.mario@example.com>
+        # Sender Mail: spoof@example.com
+
+        if msg["From"]:
+            mail_from = re.findall("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", msg["From"], re.IGNORECASE)
+            mail_from = mail_from[-1]
+        else:
+            mail_from = ""
+
+        if msg["Sender"]:
+            mail_sender = msg["Sender"]
+        else:
+            mail_sender = ""
+
+        if msg["Subject"]:
+            mail_subject = msg["Subject"]
+        else:
+            mail_subject = ""
+
+        if msg["X-Originating-IP"]:
+            mail_xorigip = msg["X-Originating-IP"]
+        else:
+            mail_xorigip = ""
+
+        data["data"].append({
+            "Filename": os.path.basename(filename),
+            "From": mail_from,
+            "Sender": mail_sender,
+            "Subject": mail_subject,
+            "X-Originating-IP": mail_xorigip,
+            "relay_full": [],
+            "relay_ip": [],
+            "url": [],
+            "domain": []
+        })
+
+        # Identify each relay
+        received = msg.get_all("Received")
+        if received:
+            received.reverse()
+            for line in received:
+                hops = re.findall("from\s+(.*?)\s+by(.*?)(?:(?:with|via)(.*?)(?:id|$)|id|$)", line, re.DOTALL | re.X)
+                for hop in hops:
+
+                    ipv4_address = re.findall(r"[0-9]+(?:\.[0-9]+){3}", hop[0], re.DOTALL | re.X)
+
+                    # https://gist.github.com/dfee/6ed3a4b05cfe7a6faf40a2102408d5d8
+                    ipv6_address = re.findall(
+                        r"(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,4}:[^\s:](?:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))|(?:::(?:ffff(?::0{1,4}){0,1}:){0,1}[^\s:](?:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))|(?:fe80:(?::(?:(?:[0-9a-fA-F]){1,4})){0,4}%[0-9a-zA-Z]{1,})|(?::(?:(?::(?:(?:[0-9a-fA-F]){1,4})){1,7}|:))|(?:(?:(?:[0-9a-fA-F]){1,4}):(?:(?::(?:(?:[0-9a-fA-F]){1,4})){1,6}))|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,2}(?::(?:(?:[0-9a-fA-F]){1,4})){1,5})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,3}(?::(?:(?:[0-9a-fA-F]){1,4})){1,4})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,4}(?::(?:(?:[0-9a-fA-F]){1,4})){1,3})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,5}(?::(?:(?:[0-9a-fA-F]){1,4})){1,2})|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,6}:(?:(?:[0-9a-fA-F]){1,4}))|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){1,7}:)|(?:(?:(?:(?:[0-9a-fA-F]){1,4}):){7,7}(?:(?:[0-9a-fA-F]){1,4}))",
+                        hop[0], re.DOTALL | re.X)
+
+                    if ipv4_address:
+                        if ipaddress.ip_address(ipv4_address[0]):
+                            if ipaddress.ip_address(ipv4_address[0]).is_private:
+                                if not exclude_private_ip:
+                                    hopListIP.append(ipv4_address[0])
+                            else:
+                                hopListIP.append(ipv4_address[0])
+
+                    if ipv6_address:
+                        if ipaddress.ip_address(ipv6_address[0]):
+                            if ipaddress.ip_address(ipv6_address[0]).is_private:
+                                if not exclude_private_ip:
+                                    hopListIP.append(ipv6_address[0])
+                            else:
+                                hopListIP.append(ipv6_address[0])
+
+                    if hop[0]:
+                        hopList.append(hop[0])
+        if hopList:
+            data["data"][0]["relay_full"].append(dict(zip(range(len(hopList)), hopList)))
+
+        if hopListIP:
+            data["data"][0]["relay_ip"].append(dict(zip(range(len(hopListIP)), hopListIP)))
+
+        if urlList:
+            data["data"][0]["url"].append(dict(zip(range(len(urlList)), urlList)))
+            data["data"][0]["domain"].append(dict(zip(range(len(domainList)), domainList)))
+
+        print(json.dumps(data, indent=4))
+
+
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", action="store", dest="file", help="Analyze an eMail (.eml format)")
+    # parser.add_argument("-d", "--directory", action="store", dest="directory",help="Analyze multiple eMails (.eml format) contained in a directory.")
+    parser.add_argument("-x", "--exclude-private-ip", action="store_true", dest="excprip",
+                        help="Exclude private IPs from the report")
+    # parser.add_argument("-v", "--version", action="version", version="%(prog)s " + swversion)
+
+    arguments = parser.parse_args()
+
+    if arguments.file:
+        email_analysis(arguments.file, arguments.excprip)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
