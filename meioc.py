b39eba9e9486adad2568a3a709a54aa758c592eb
Andre Draghetti <drego85@draghetti.it>
2019-10-30 22:16:06 +0100

    Release of version 1.2

--- meioc.py
+++ meioc.py
@@ -30,64 +30,84 @@ def email_analysis(filename, exclude_private_ip, check_spf):
     hopList = []
     hopListIP = []
     domainList = []
+    attachmentsList = []
     hopListIPnoPrivate = []
+
+    resultmeioc = {
+        "filename": os.path.basename(filename),
+        "from": None,
+        "sender": None,
+        "x-sender": None,
+        "to": None,
+        "cc": None,
+        "bcc": None,
+        "envelope-to": None,
+        "delivered-to": None,
+        "subject": None,
+        "x-originating-ip": None,
+        "relay_full": None,
+        "relay_ip": None,
+        "spf": None,
+        "urls": None,
+        "domains": None,
+        "attachments": None
+    }
 
     with open(filename, "rb") as fp:
         msg = BytesParser(policy=policy.default).parse(fp)
 
     if msg:
-        # A sender obfuscation technique involves entering two e-mails. Only the last one is the real one. Example:
+
         #
-        # Sender Name: Mario Rossi <rossi.mario@example.com>
-        # Sender Mail: spoof@example.com
+        # Header analysis
+        #
 
         if msg["From"]:
+            # A sender obfuscation technique involves entering two e-mails. Only the last one is the real one. Example:
+            #
+            # Sender Name: Mario Rossi <rossi.mario@example.com>
+            # Sender Mail: spoof@example.com
             mail_from = re.findall("[A-Za-z0-9.!#$%&'*+\/=?^_`{|}~\-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", msg["From"],
                                    re.IGNORECASE)
 
             if mail_from:
-                mail_from = mail_from[-1]
-            else:
-                mail_from = ""
-        else:
-            mail_from = ""
+                resultmeioc["from"] = mail_from[-1]
 
         if msg["Sender"]:
-            mail_sender = msg["Sender"]
-        else:
-            mail_sender = ""
+            mail_sender = re.findall("[A-Za-z0-9.!#$%&'*+\/=?^_`{|}~\-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", msg["Sender"],
+                                     re.IGNORECASE)
+
+            if mail_sender:
+                resultmeioc["sender"] = mail_sender[-1]
 
         if msg["X-Sender"]:
-            mail_xsender = msg["X-Sender"]
-        else:
-            mail_xsender = ""
+            mail_xsender = re.findall("[A-Za-z0-9.!#$%&'*+\/=?^_`{|}~\-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}",
+                                      msg["X-Sender"],
+                                      re.IGNORECASE)
+
+            if mail_xsender:
+                resultmeioc["x-sender"] = mail_xsender[-1]
 
         if msg["To"]:
             mail_to = re.findall("[A-Za-z0-9.!#$%&'*+\/=?^_`{|}~\-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", msg["To"],
                                  re.IGNORECASE)
 
             if mail_to:
-                mail_to = dict(zip(range(len(mail_to)), mail_to))
-            else:
-                mail_to = ""
-        else:
-            mail_to = ""
+                # Remove possible duplicates and create a numbered dictionary
+                mail_to = dict(zip(range(len(list(set(mail_to)))), list(set(mail_to))))
+                resultmeioc["to"] = mail_to
 
         if msg["Bcc"]:
-            mail_bcc = msg["Bcc"]
-        else:
-            mail_bcc = ""
+            resultmeioc["bcc"] = msg["Bcc"]
 
         if msg["Cc"]:
             mail_cc = re.findall("[A-Za-z0-9.!#$%&'*+\/=?^_`{|}~\-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}", msg["Cc"],
                                  re.IGNORECASE)
 
             if mail_cc:
-                mail_cc = dict(zip(range(len(mail_cc)), mail_cc))
-            else:
-                mail_cc = ""
-        else:
-            mail_cc = ""
+                # Remove possible duplicates and create a numbered dictionary
+                mail_cc = dict(zip(range(len(list(set(mail_cc)))), list(set(mail_cc))))
+                resultmeioc["cc"] = mail_cc
 
         if msg["Envelope-to"]:
 
@@ -96,81 +116,20 @@ def email_analysis(filename, exclude_private_ip, check_spf):
                                          re.IGNORECASE)
 
             if mail_envelopeto:
-                mail_envelopeto = dict(zip(range(len(mail_envelopeto)), mail_envelopeto))
-            else:
-                mail_envelopeto = ""
-        else:
-            mail_envelopeto = ""
+                # Remove possible duplicates and create a numbered dictionary
+                mail_envelopeto = dict(zip(range(len(list(set(mail_envelopeto)))), list(set(mail_envelopeto))))
+                resultmeioc["envelope-to"] = mail_envelopeto
 
         if msg["Delivered-To"]:
-            mail_deliveredto = msg["Delivered-To"]
-        else:
-            mail_deliveredto = ""
+            resultmeioc["delivered-to"] = msg["Delivered-To"]
 
         if msg["X-Originating-IP"]:
             # Usually the IP is in square brackets, I remove them if present.
             mail_xorigip = msg["X-Originating-IP"].replace("[", "").replace("]", "")
-        else:
-            mail_xorigip = ""
+            resultmeioc["x-originating-ip"] = mail_xorigip
 
         if msg["Subject"]:
-            mail_subject = msg["Subject"]
-        else:
-            mail_subject = ""
-
-        resultmeioc = {
-            "filename": os.path.basename(filename),
-            "from": mail_from,
-            "sender": mail_sender,
-            "x-sender": mail_xsender,
-            "to": mail_to,
-            "cc": mail_cc,
-            "bcc": mail_bcc,
-            "envelope-to": mail_envelopeto,
-            "delivered-to": mail_deliveredto,
-            "subject": mail_subject,
-            "x-originating-ip": mail_xorigip,
-        }
-
-        # Identify each url or attachment reported in the eMail body
-        for part in msg.walk():
-            if part.get_content_type() == "text/plain":
-                # https://gist.github.com/dperini/729294
-                urlList.extend(re.findall(
-                    "(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?",
-                    part.get_content(), re.UNICODE | re.IGNORECASE | re.MULTILINE))
-
-            if part.get_content_type() == "text/html":
-                # The try/except is necessary, if the body of the eMail contains an incorrect or unencoded HTML code the script freeezes.
-                try:
-                    soup = BeautifulSoup(part.get_content(), "html.parser")
-                    tags = soup.find_all("a", href=True)
-                    for url in tags:
-                        urlList.append(url.get("href"))
-                except:
-                    pass
-
-            if part.get_filename():
-                if part.get_filename():
-                    if part.get_payload(decode=True):
-                        filename = part.get_filename()
-                        filemd5 = hashlib.md5(part.get_payload(decode=True)).hexdigest()
-                        filesha1 = hashlib.sha1(part.get_payload(decode=True)).hexdigest()
-                        filesha256 = hashlib.sha256(part.get_payload(decode=True)).hexdigest()
-
-                        resultmeioc.update(
-                            {"attachments": {"filename": filename, "MD5": filemd5, "SHA1": filesha1,
-                                             "SHA256": filesha256}})
-
-        # Identify each domain reported in the eMail body
-        for url in urlList:
-            analyzeddomain = tldcache(url).registered_domain
-            if analyzeddomain:
-                domainList.append(analyzeddomain)
-
-        # Remove Duplicate
-        urlList = list(set(urlList))
-        domainList = list(set(domainList))
+            resultmeioc["subject"] = msg["Subject"]
 
         # Identify each relay
         received = msg.get_all("Received")
@@ -207,26 +166,72 @@ def email_analysis(filename, exclude_private_ip, check_spf):
                         hopList.append(hop[0])
 
         if hopList:
-            resultmeioc.update({"relay_full": dict(zip(range(len(hopList)), hopList))})
+            resultmeioc["relay_full"] = dict(zip(range(len(hopList)), hopList))
 
         if hopListIP:
             if exclude_private_ip:
-                resultmeioc.update({"relay_ip": dict(zip(range(len(hopListIPnoPrivate)), hopListIPnoPrivate))})
+                resultmeioc["relay_ip"] = dict(zip(range(len(hopListIPnoPrivate)), hopListIPnoPrivate))
             else:
-                resultmeioc.update({"relay_ip": dict(zip(range(len(hopListIP)), hopListIP))})
+                resultmeioc["relay_ip"] = dict(zip(range(len(hopListIP)), hopListIP))
+
+        #
+        # Body analysis
+        #
+        for part in msg.walk():
+            if part.get_content_type() == "text/plain":
+                # https://gist.github.com/dperini/729294
+                urlList.extend(re.findall(
+                    "(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?",
+                    part.get_content(), re.UNICODE | re.IGNORECASE | re.MULTILINE))
+
+            if part.get_content_type() == "text/html":
+                # The try/except is necessary, if the body of the eMail contains an incorrect or unencoded HTML code the script freeezes.
+                try:
+                    soup = BeautifulSoup(part.get_content(), "html.parser")
+                    tags = soup.find_all("a", href=True)
+                    for url in tags:
+                        urlList.append(url.get("href"))
+                except:
+                    pass
+
+            if part.get_filename():
+                if part.get_payload(decode=True):
+                    filename = part.get_filename()
+                    filemd5 = hashlib.md5(part.get_payload(decode=True)).hexdigest()
+                    filesha1 = hashlib.sha1(part.get_payload(decode=True)).hexdigest()
+                    filesha256 = hashlib.sha256(part.get_payload(decode=True)).hexdigest()
+
+                    attachmentsList.append({"filename": filename, "MD5": filemd5, "SHA1": filesha1,
+                                            "SHA256": filesha256})
+
+        # Identify each domain reported in the eMail body
+        for url in urlList:
+            analyzeddomain = tldcache(url).registered_domain
+            if analyzeddomain:
+                domainList.append(analyzeddomain)
+
+        # Remove Duplicate
+        urlList = list(set(urlList))
+        domainList = list(set(domainList))
 
         if urlList:
-            resultmeioc.update({"urls": dict(zip(range(len(urlList)), urlList))})
-            resultmeioc.update({"domains": dict(zip(range(len(domainList)), domainList))})
+            resultmeioc["urls"] = dict(zip(range(len(urlList)), urlList))
+            resultmeioc["domains"] = dict(zip(range(len(domainList)), domainList))
 
+        if attachmentsList:
+            resultmeioc["attachments"] = attachmentsList
+
+        #
         # Verify the SPF record if requested
+        #
         if check_spf:
             testspf = False
             resultspf = ""
             for ip in hopListIPnoPrivate:
-                if not testspf and mail_from:
+                if not testspf and "mail_from" in locals():
+                    resultspf = spf.check2(ip, mail_from[-1], mail_from[-1].split("@")[1])[0]
                     try:
-                        resultspf = spf.check2(ip, mail_from, mail_from.split("@")[1])[0]
+                        resultspf = spf.check2(ip, mail_from[-1], mail_from[-1].split("@")[1])[0]
                     except:
                         pass
 
@@ -235,13 +240,13 @@ def email_analysis(filename, exclude_private_ip, check_spf):
                     else:
                         testspf = False
 
-            resultmeioc.update({"spf": testspf})
+            resultmeioc["spf"] = testspf
 
         print(json.dumps(resultmeioc, indent=4))
 
 
 def main():
-    version = "1.1"
+    version = "1.2"
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="Analyze an eMail (.eml format)")
     parser.add_argument("-x", "--exclude-private-ip", action="store_true", dest="excprip",
